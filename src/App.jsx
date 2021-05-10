001eddd310b19befb7ba61c7216740506bbd7b8e
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-10 18:19:57 +0200

    Add Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import Button from './Button'
+import Pagination from './Pagination'
 export default () => {
   return (
     <div>
-      <Button isActive>Moin there</Button>
-      <Button isActive={false}>Not active</Button>
+      <Pagination isLeftDisabled text="1/34" />
+      <Pagination isRightDisabled text="2/34" />
+      <Pagination isLeftDisabled text="34/34" />
+      <Button isActive={true}>Active</Button>
+      <Button>Not Active</Button>
     </div>
   )
 }
