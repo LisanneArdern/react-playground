d052ddfae76a1523343cde700ff1c04054bcea8f
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-11 15:45:39 +0200

    Add Array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Card from './Card'
+
+const cards = [
+  { title: 'Name', text: 'Lisanne', isActive: false, id: 1 },
+  { title: 'Name', text: 'Isabel', isActive: true, id: 1 },
+]
 export default () => {
   return (
-    <div>
-      <Card isActive text="Lisanne" title="Name" />
-      <Card text="26" title="Age" />
+    <div className="App">
+      {cards.map(({ title, text, isActive, id }) => (
+        <Card key={id} title={title} text={text} isActive={isActive} />
+      ))}
     </div>
   )
 }
